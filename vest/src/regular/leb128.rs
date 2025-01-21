02d3463f00803042581514b68ff0adb31a8cd77c
Pratap Singh <pratapsingh1729@gmail.com>
2025-01-21 18:44:02 -0500

    recursive parser for leb128

--- vest/src/regular/leb128.rs
+++ vest/src/regular/leb128.rs
@@ -239,6 +239,37 @@ impl UnsignedLEB128 {
             }
         }
     }
+
+
+    fn exec_parse_rec_helper(&self, s: &[u8]) -> (res: PResult<UInt, ParseError>)
+        ensures
+            res matches Ok((n, v)) ==> self@.spec_parse(s@) == Ok::<_, ()>((n, v@)) && n
+                <= s@.len(),
+            self@.spec_parse(s@) matches Ok((m, w)) ==> res matches Ok((m, v)) && w == v@,
+            res is Err ==> self@.spec_parse(s@) is Err,
+            self@.spec_parse(s@) is Err ==> res is Err,
+    {
+        if s.len() != 0 {
+            let v = take_low_7_bits!(s[0]);
+            if is_high_8_bit_set!(s[0]) {
+                match self.exec_parse_rec_helper(s.subrange(1, s.len())) {
+                    Ok((n, v2)) => {
+                        // Check for overflow and canonicity (v2 should not be 0)
+                        if n < usize::MAX && 0 < v2 && v2 <= n_bit_max_unsigned!(8 * uint_size!() - 7) {
+                            Ok(((n + 1) as usize, v2 << 7 | v as UInt))
+                        } else {
+                            Err(ParseError::Other("Failed to parse LEB128: overflow or canonicity violation".to_string()))
+                        }
+                    }
+                    Err(e) => Err(e),
+                }
+            } else {
+                Ok((1, v as UInt))
+            }
+        } else {
+            Err(ParseError::UnexpectedEndOfInput)
+        }
+    }
 }
 
 impl SecureSpecCombinator for UnsignedLEB128 {
@@ -354,53 +385,56 @@ impl<I,O> Combinator<I,O> for UnsignedLEB128
         true
     }
 
-
     fn parse(&self, ss: I) -> (res: PResult<Self::Type, ParseError>) {
-        let s = ss.as_byte_slice();
-        let mut result: Self::Type = 0;
-        let mut shift = 0;
-        let mut i = 0;
-
-        if s.len() == 0 {
-            return Err(ParseError::UnexpectedEndOfInput);
-        }
-        let ghost spec_res = self.spec_parse(s@);
-        proof { admit() };
-
-        while i < s.len()
-            invariant 
-                s.len() != 0,
-                s@ == ss@,
-                shift == i * 7,
-                0 <= i < 10,
-                spec_res == self.spec_parse(s@),
-                // spec_res matches Ok((spec_res_n, spec_res_v)) ==> {
-                //     &&& i <= spec_res_n
-                //     &&& result == 
-                    
-                // }
-                self.spec_parse(s@.take(i as int)) matches Ok((j, spec_res)) ==> {
-                    &&& result == spec_res
-                    &&& j == i
-                }
-            decreases s.len() - i
-        {
-            proof { 
-                self.lemma_spec_parse_length(s@);
-                self.lemma_spec_parse_length_bound(s@);
-            }
-            proof { admit() };
-
-            let byte = s[i];
-            result |= (take_low_7_bits!(byte) as Self::Type) << shift;
-            shift += 7;
-            i += 1;
-            if !is_high_8_bit_set!(byte) {
-                return Ok((i, result));
-            }
-        }
-        Err(ParseError::UnexpectedEndOfInput)
+        self.exec_parse_rec_helper(ss.as_byte_slice())
     }
+
+    // fn parse(&self, ss: I) -> (res: PResult<Self::Type, ParseError>) {
+    //     let s = ss.as_byte_slice();
+    //     let mut result: Self::Type = 0;
+    //     let mut shift = 0;
+    //     let mut i = 0;
+
+    //     if s.len() == 0 {
+    //         return Err(ParseError::UnexpectedEndOfInput);
+    //     }
+    //     let ghost spec_res = self.spec_parse(s@);
+    //     proof { admit() };
+
+    //     while i < s.len()
+    //         invariant 
+    //             s.len() != 0,
+    //             s@ == ss@,
+    //             shift == i * 7,
+    //             0 <= i < 10,
+    //             spec_res == self.spec_parse(s@),
+    //             // spec_res matches Ok((spec_res_n, spec_res_v)) ==> {
+    //             //     &&& i <= spec_res_n
+    //             //     &&& result == 
+                    
+    //             // }
+    //             self.spec_parse(s@.take(i as int)) matches Ok((j, spec_res)) ==> {
+    //                 &&& result == spec_res
+    //                 &&& j == i
+    //             }
+    //         decreases s.len() - i
+    //     {
+    //         proof { 
+    //             self.lemma_spec_parse_length(s@);
+    //             self.lemma_spec_parse_length_bound(s@);
+    //         }
+    //         proof { admit() };
+
+    //         let byte = s[i];
+    //         result |= (take_low_7_bits!(byte) as Self::Type) << shift;
+    //         shift += 7;
+    //         i += 1;
+    //         if !is_high_8_bit_set!(byte) {
+    //             return Ok((i, result));
+    //         }
+    //     }
+    //     Err(ParseError::UnexpectedEndOfInput)
+    // }
 
     open spec fn serialize_requires(&self) -> bool {
         true
@@ -414,6 +448,7 @@ impl<I,O> Combinator<I,O> for UnsignedLEB128
         let ghost orig_v = v;
         let ghost spec_res = self.spec_serialize(v);
         proof { self.lemma_spec_serialize_length(v) }
+        proof { admit() };
 
         assert(v == orig_v >> 0) by (bit_vector) requires v == orig_v;
 
@@ -423,12 +458,12 @@ impl<I,O> Combinator<I,O> for UnsignedLEB128
                 buf@.len() == old(buf)@.len(),
                 v == orig_v >> (i * 7),
                 self.spec_serialize(orig_v) matches Ok(s) ==> {
-                    &&& s.len() <= 10
+                    &&& 0 <= i < s.len() <= 10
                     &&& s.subrange(0, i as int) == buf@.subrange(pos as int, pos + i as int)
                 } 
             decreases v
         {
-            // proof { admit() };
+            proof { admit() };
             let lo = take_low_7_bits!(v);
             let hi = v >> 7;
             let byte = if hi == 0 { lo } else { set_high_8_bit!(lo) };
